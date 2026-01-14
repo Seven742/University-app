0264674b264289a68a17f26365b264e1a577a6c8
Seven742 <kimsean0041@gmail.com>
2026-01-14 10:23:28 +0700

    Update Page.jsx

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image1 from '../assets/image1.jpg'
+import Image1 from '../assets/Image1.jpg'
 import { GiStabbedNote } from "react-icons/gi";
 import { FaUniversity } from "react-icons/fa";
 import { GiWorld } from "react-icons/gi";
